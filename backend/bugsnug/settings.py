2257a2861e6bc365b40d826b4f78bd2d50f32e0f
Aterolite24 <aterolite24@gmail.com>
2026-02-07 17:00:33 +0530

    config: Set production API URL for frontend and enable all CORS origins for backend.

--- backend/bugsnug/settings.py
+++ backend/bugsnug/settings.py
@@ -93,10 +93,11 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
